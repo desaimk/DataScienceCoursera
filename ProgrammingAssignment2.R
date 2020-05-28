eaba7118e8469319099d9da6f87200b647584b19
desaimk <65666641+desaimk@users.noreply.github.com>
2020-05-28 14:57:19 -0400

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,32 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+ a <- NULL
+ set <- function(b) {
+     x <<- b
+     a <<- NULL
+ }
+ get <- function() x
+ inverse <- function(inverse) a <<- inverse
+ getinv <- function () a
+ list(set = set, get = get, inverse = inverse, getinv = getinv)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+    a <- x$getinv()
+    if (!is.null(a)) {
+        message("getting cached data")
+        return(a)
+    }
+    data <- x$get()
+    a <- solve(data, ...)
+    x$inverse(a)
+    a
+        ## Return a matrix that is the inverse of 'x'
+}
